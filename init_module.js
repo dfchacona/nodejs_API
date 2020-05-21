a2990b010a6819767719237fe24cf12de0a0fe34
dieguischa <dfchacona@unal.edu.co>
2020-05-21 12:40:23 -0500

    database connection :hammer:

--- init_module.js
+++ init_module.js
@@ -1,7 +1,12 @@
-var http = require('http');
+var mysql = require('mysql');
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write("Hello World!");
-  res.end();
-}).listen(8080);
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  database : 'api_db'
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
