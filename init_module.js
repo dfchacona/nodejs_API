e471466919be5215fbe3baafc84ae109a04aabc8
dieguischa <dfchacona@unal.edu.co>
2020-05-21 15:07:54 -0500

    added validators :hammer:

--- init_module.js
+++ init_module.js
@@ -1,6 +1,7 @@
 var mysql = require('mysql');
 var url = require('url');
 var http = require('http');
+var validator = require('validator');
 
 //create a server object:
 http.createServer(function (req, res) {
@@ -32,15 +33,40 @@ http.createServer(function (req, res) {
 //   ENDPOINT ADD
   if(q.pathname=="/activities/add"){
     var q = url.parse(req.url, true).query;
-
+    var valid = true;
+    var err_log = "";
+    trimmed_name=q.name.replace(/\s/g, "");
+    console.log(trimmed_name);
+    if(validator.default.isEmpty(trimmed_name) || validator.default.isEmpty(q.salary)){
+        // res.writeHead(400, {'Content-Type': 'text'});
+        err_log += 'fill both params\n';
+        valid=false;
+    }else{
+    if(!validator.default.isAlpha(trimmed_name,'en-US')){
+        err_log += 'name must be text\n';
+        valid=false;
+    }
+    if(!validator.default.isNumeric(q.salary,'en-US')){
+        err_log +='salary must be numeric';
+        valid=false;
+    }}
+    
     // INSERT INTO `activities` VALUES (1,'architect',1000)
+    if(valid){
     var sql = "INSERT INTO activities (name, salary) VALUES ('"+q.name+"', '"+q.salary+"')";
     con.query(sql, function (err, result) {
         if (err) throw err;
         console.log("Record inserted");
-        res.write("name: "+q.name+", salary:"+q.salary);
-        res.end();
       });
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.write("name: "+q.name+", salary:"+q.salary);
+      res.end();
+    }else{
+        res.writeHead(400, {'Content-Type': 'text/html'});
+        res.write(err_log);
+        res.end();
+    }
+    
   }
   
   
