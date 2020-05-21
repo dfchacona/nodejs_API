cc896c492bb62904896b4fe5202872858c32d413
dieguischa <dfchacona@unal.edu.co>
2020-05-21 13:46:36 -0500

    Show activities end point

--- init_module.js
+++ init_module.js
@@ -1,12 +1,35 @@
 var mysql = require('mysql');
+var url = require('url');
+var http = require('http');
 
-var con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  database : 'api_db'
-});
+//create a server object:
+http.createServer(function (req, res) {
+  var q = url.parse(req.url, true);
+  var con = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        database : 'api_db'
+      });
 
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-});
+
+  con.connect(function(err) {
+        if (err) throw err;
+        console.log("Connected!");
+  });
+
+
+  if(q.pathname=="/activities/index"){
+    con.query("SELECT * FROM activities", function (err, result, fields) {
+        if (err) throw err;
+        
+        var string=JSON.stringify(result);
+        res.write(string);
+        res.end();
+      });
+  }
+  
+  
+}).listen(8080); //the server object listens on port 8080
+
+
+
